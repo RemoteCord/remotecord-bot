ba98bf4a5293bf83a112ad66f4d9a3ce9ff6170d
luqueee <luqueee_@outlook.es>
2025-03-27 09:32:37 +0100

    enhance ecosystem configuration: change script execution to use npm start for remotecord-bot

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,18 @@
 module.exports = {
-  apps: [{
-    name: 'remotecord-bot',
-    script: './dist/index.js',
-    watch: false,
-    max_memory_restart: '1G',
-    exec_mode: 'fork',
-    instances: 1,
-    autorestart: true,
-    max_restarts: 10,
-    env: {
-      NODE_ENV: 'production'
-    }
-  }]
-}
+	apps: [
+		{
+			name: "remotecord-bot",
+			script: "npm",
+			args: "run start",
+			watch: false,
+			max_memory_restart: "1G",
+			exec_mode: "fork",
+			instances: 1,
+			autorestart: true,
+			max_restarts: 10,
+			env: {
+				NODE_ENV: "production"
+			}
+		}
+	]
+};
